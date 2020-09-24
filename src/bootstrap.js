3ac6f5d6842d0aa52d085018389ff6a1854a1eae
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-24 17:01:21 -0500

    wrapped components into requireAuth so user can only access them when authenticated

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import { StateProvider } from "./components/StateProvider";
+import history from "./history";
+import requireAuth from "./components/requireAuth";
 import reducer, { initialState } from "./components/reducer";
 import Icons from "./components/Icons";
 import Layout from "./components/Layout";
@@ -39,31 +41,35 @@ function Main() {
 
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <StateProvider initialState={initialState} reducer={reducer}>
             <Layout>
               <Route path="/" exact component={Home} />
               <Route path="/signin" exact component={SignIn} />
               <Route path="/signup" exact component={SignUp} />
-              <Route path="/cart" exact component={Cart} />
+              <Route path="/cart" exact component={requireAuth(Cart)} />
               <Route
                 path="/shipping-address"
                 exact
-                component={ShippingAdress}
+                component={requireAuth(ShippingAdress)}
               />
               <Elements stripe={stripePromise}>
                 <Route
                   path="/order-details"
                   exact
-                  component={OrderDetailsContainer}
+                  component={requireAuth(OrderDetailsContainer)}
                 />
               </Elements>
-              <Route path="/order-success" exact component={OrderSuccess} />
+              <Route
+                path="/order-success"
+                exact
+                component={requireAuth(OrderSuccess)}
+              />
             </Layout>
           </StateProvider>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>,
     document.querySelector(".app-wrapper")
   );
