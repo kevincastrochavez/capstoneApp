be4dffe0afdf6f2682ed3065e76138fc0b18401b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-24 16:21:42 -0500

    added functionality so when click on logout, refreshes the page

--- src/components/LogOutLink.js
+++ src/components/LogOutLink.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function LogOutLink() {
   return (
     <div>
-      <Link to="/forgot" className="navbar__link">
+      <Link onClick={() => window.location.reload()} className="navbar__link">
         <div className="navbar__option">
           <span>Log Out</span>
         </div>
